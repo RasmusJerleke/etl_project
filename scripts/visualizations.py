7054145b68622dd2bd55bb341b621cc6353e3a12
marihanson <hmarianne1@yahoo.com>
2022-07-14 10:03:47 +0200

    axis x lable format

--- scripts/visualizations.py
+++ scripts/visualizations.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 city = 'data'
+paramenter = 'temperature'
 #for future iteration 
 #for city in geo_locations.keys()
 
@@ -24,9 +25,13 @@ def forecast_vis_line(city, parameter):
     mean = group_data_mean(city)[parameter]
     max = group_data_max(city)[parameter]
     min = group_data_min(city)[parameter]
+    ax= plt.subplot()
     plt.plot(mean.index, mean, marker = 'o')
-    plt.plot(max.index, max, marker = 'o')
-    plt.plot(max.index, min, marker = 'o')
+    plt.plot(max.index, max, 'o:')
+    plt.plot(max.index, min, 'o:')
+    plt.setp
+    plt.setp(ax.get_xticklabels(), rotation=30, ha='right')
     plt.title(f"Daily {parameter} forecast for {city} - mean, max and min")
     plt.savefig(f'visualizations/{city}_{parameter}.png')
+
 
